e8a1ac5581d6b73344ba56a000f2ed5ba342726d
Bond <jobocuban@gmail.com>
2019-09-30 22:51:13 -0600

    Initial commit

---
+++ src/Example2.java
@@ -0,0 +1,23 @@
+/*
+This demonstrates a variable
+
+Call this file Example2.java.
+ */
+
+public class Example2 {
+
+  public static void main(String[] args) {
+    int myVar1; // This declares a variable.
+    int myVar2; // This declares another variable.
+
+    myVar1 = 1024; // this assigns 1024 to myVar1
+
+    System.out.println("myVar1 contains " + myVar1);
+
+    myVar2 = myVar1 / 2;
+
+    System.out.print("myVar2 contains myVar1 / 2: ");
+    System.out.println(myVar2);
+  }
+
+}
